2fe8d7c3ad61d951bf0666bda838d8e1f973f03f
N0iSy Yeager <n0isyb0y1658@gmail.com>
2024-05-14 14:39:12 +0630

    Add project files.

---
+++ BuildingManagement/Data/BuilidngDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BuildingManagement.Models;
+using Microsoft.EntityFrameworkCore;
+namespace BuildingManagement.Data
+{
+    public class BuildingDbContext : DbContext
+    {
+        public BuildingDbContext(DbContextOptions<BuildingDbContext> options) : base(options) { }
+
+        public DbSet<Company> MS_Company { get; set; }
+        public DbSet<RoomProperty> MS_PropertyRoom { get; set; } = default!;
+    }
+}
+
---
+++ BuildingManagement/Models/RoomProperty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+
+namespace BuildingManagement.Models
+{
+    public class RoomProperty
+    {
+        [Key]
+        public int RoomId { get; set; }
+        public required string RoomNo { get; set; }
+        public int LocId { get; set; }
+        public int BdTypId { get; set; }
+        public float? SqFullMeasure { get; set; }
+        public float? SqRoomeasure { get; set; }
+        public string? AmenityDesc { get; set; }
+        public string? FeatureDesc { get; set; }
+        public string? Addr { get; set; }
+        public int? TenantId { get; set; }
+        public short CmpyId { get; set; }
+        public int UserId { get; set; }
+        public DateTime RevDteTime { get; set; }
+    }
+}
