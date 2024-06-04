2aeacf16cd7bbc5f11f30cb5006f8b0b8a2cc8a2
SandarHlaing <sandarhlaing858@gmail.com>
2024-06-04 10:43:41 +0630

    merge by KIKI

---
+++ BuildingManagement/Common/Encrypt.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BuildingManagement.Common
+{ 
+    public class Encrypt
+
+    {
+        public string EncryptString(string plainInput)
+        {
+            var key = "tkbH1omfiqg13aqVusoCialf7pE6whfU";
+            var iv = new byte[16];
+            byte[] array;
+            using var aes = Aes.Create();
+
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = iv;
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            using var memoryStream = new MemoryStream();
+            using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+            using (var streamWriter = new StreamWriter(cryptoStream))
+            {
+                streamWriter.Write(plainInput);
+            }
+
+            array = memoryStream.ToArray();
+
+
+            return Convert.ToBase64String(array);
+        }
+
+        public string DecryptString(string cipherText)
+        {
+            var key = "tkbH1omfiqg13aqVusoCialf7pE6whfU";
+            var iv = new byte[16];
+            var buffer = Convert.FromBase64String(cipherText);
+
+            using var aes = Aes.Create();
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = iv;
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using var memoryStream = new MemoryStream(buffer);
+            using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            using var streamReader = new StreamReader(cryptoStream);
+
+            return streamReader.ReadToEnd();
+        }
+
+        internal string EncryptString(object password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
