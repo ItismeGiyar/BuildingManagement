0f0bc3091c619a5bdb19ef08c3803e20d9e9d074
N0iSy Yeager <n0isyb0y1658@gmail.com>
2024-07-25 14:01:40 +0700

    add by kg zin

--- BuildingManagement/Models/ComplaintCatg.cs
+++ BuildingManagement/Models/ComplaintCatg.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
